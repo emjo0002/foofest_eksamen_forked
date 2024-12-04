dd75502866b3bb69180f6463b80e41064380fbc2
Emil <emjo0002@stud.kea.dk>
2024-12-04 12:56:51 +0100

    .

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="/">Hjem</a></li>
+          <li><a href="/about">Om os</a></li>
+          <li><a href="/contact">Kontakt</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
