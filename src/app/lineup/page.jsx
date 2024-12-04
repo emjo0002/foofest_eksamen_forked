d8a3e171792a01b3f7ef8a8afb9a8e7c18c86ba8
Emil <emjo0002@stud.kea.dk>
2024-12-04 16:14:50 +0100

    kommet langt weeeeee!

--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { getAllBands } from "../lib/api";
 
@@ -20,7 +21,7 @@ export default function LineupPage() {
          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
            {bands.map((band) => (
           <div key={band.id} className="bg-blue-800 p-4 rounded-lg shadow-lg hover:shadow-2xl transition duration-300">
-            <img src={band.image} alt={band.name} className="w-full h-64 object-cover rounded-md" />
+            <Image src={`/logos/${band.logo}`} width={250} height={250} alt={band.title} className="w-full h-64 object-cover rounded-md" />
             <h2 className="text-2xl font-semibold mt-4">{band.name}</h2>
             <p className="mt-2">{band.description}</p>
             <button className="mt-4 bg-white text-blue-900 px-4 py-2 rounded-lg">More Info</button>
