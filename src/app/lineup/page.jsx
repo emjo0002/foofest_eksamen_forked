3d6180ea2431b8a38f66f1018a3ab9e087bc30a9
Emil <emjo0002@stud.kea.dk>
2024-12-05 11:41:50 +0100

    .

--- src/app/lineup/page.jsx
+++ src/app/lineup/page.jsx
@@ -17,20 +17,33 @@ export default function LineupPage() {
   }, []);
 
   return (
-    <div className="bg-blue-900 text-white min-h-screen px-8 py-12">
-         <h1 className="text-4xl font-bold mb-8">LINE-UP</h1>
-         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-           {bands.map((band) => (
-          <div key={band.id} className="bg-blue-800 p-4 rounded-lg shadow-lg hover:shadow-2xl transition duration-300">
-            <Image src={`/logos/${band.logo}`} width={250} height={250} alt={band.slug} className="w-full h-64 object-cover rounded-md" />
+  <div className="bg-blue-900 text-white min-h-screen px-8 py-12">
+    <h1 className="text-4xl font-bold mb-8">LINE-UP</h1>
+    <div className="wrapper grid overflow-x-auto overscroll-contain scrollbar-hide scroll-snap-x mandatory py-8">
+      <div className="items flex gap-4 pl-[calc(50vw-12rem)] pr-[calc(50vw-12rem)]">
+        {bands.map((band) => (
+          <div
+            key={band.id}
+            className="bg-blue-800 grid place-content-center rounded-lg shadow-lg hover:shadow-2xl transition duration-300 scroll-snap-center w-96"
+          >
+            <Image
+              src={`/logos/${band.logo}`}
+              width={275}
+              height={250}
+              alt={band.slug}
+              className="h-64 object-fill rounded-md"
+            />
             <h2 className="text-2xl font-semibold mt-4">{band.name}</h2>
             <p className="mt-2">{band.description}</p>
-             <Link className="text-black" href={`/artist/${band.slug}`} name={band.name}>
-              <button className="mt-4 bg-white text-blue-900 px-4 py-2 rounded-lg">More Info</button>        
+            <Link href={`/artist/${band.slug}`} name={band.name}>
+              <button className="mt-4 bg-white text-blue-900 px-4 py-2 rounded-lg">
+                More Info
+              </button>
             </Link>
           </div>
         ))}
       </div>
     </div>
-  );
+  </div>
+);
 }
