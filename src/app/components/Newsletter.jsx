1f4f87fcd5ba366c28072a1da9b49c82f1f67a32
Leah <lepa0001@stud.kea.dk>
2024-12-19 12:45:09 +0100

    footer

--- src/app/components/Newsletter.jsx
+++ src/app/components/Newsletter.jsx
@@ -1,12 +1,11 @@
 function Newsletter() {
   return (
     <div className="max-w-md mx-auto p-4">
-      {/* <h2 className="text-center text-black font-bold mb-4">Tilmeld dig vores nyhedsbrev og vær de første til at modtage nyheder om nye kollektioner og eksklusive tilbud!</h2> */}
-      <div className="flex border-2 border-black">
-        <input type="email" placeholder="Tilmeld dig vores nyhedsbrev.." className="w-3/4 px-4 py-2 text-black text-lg outline-none" />
-        <button className="w-1/4 bg-slate-900 text-white text-lg">SIGN UP</button>
+      <h2 className="text-white text-3xl font-bold font-genos mb-4">NYHEDSBREV</h2>
+      <div className="flex items-center border-b-2 border-white">
+        <input type="email" placeholder="Skriv din E-mail her" className="w-full bg-transparent text-white placeholder-white py-2 text-lg font-genos outline-none" />
+        <button className="ml-2 text-white text-2xl">&rarr;</button>
       </div>
-      {/* <p className="mt-4 text-black text-sm text-center">Ved at tilmelde dig giver du samtykke til, at Foofestival sender dig nyhedsbreve. Vi deler aldrig dine oplysninger med tredjeparter. Du kan til enhver tid afmelde dig via linket i vores e-mails.</p> */}
     </div>
   );
 }
