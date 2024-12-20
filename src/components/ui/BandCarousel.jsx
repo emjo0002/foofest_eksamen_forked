2022cae76dc19b09a446c3232236e7eb8ce4e39a
Leah <lepa0001@stud.kea.dk>
2024-12-20 02:28:35 +0100

    random billeder på lineup

--- src/components/ui/BandCarousel.jsx
+++ src/components/ui/BandCarousel.jsx
@@ -14,16 +14,14 @@ export default function BandCarousel({ bands }) {
     <Carousel showThumbs={false} infiniteLoop={false} interval={3000} showStatus={false} showIndicators={false} onChange={(index) => setCurrentSlide(index)}>
       {bands.map((band, index) => (
         <div key={band.id || index} className={`text-center rounded-lg p-4 m-4 transition duration-300 ${currentSlide === index ? "scale-110" : "scale-75 opacity-65"}`}>
-          <Image src={`/logos/${band.logo}`} width={100} height={100} alt={band.name} className="h-96 object-contain rounded-md mx-auto" />
+          <Image src={band.logo?.startsWith("http") ? band.logo : `/logos/${band.logo}`} width={275} height={250} alt={band.slug || band.name} className="h-96 object-contain rounded-md mx-auto" />
           <h2 className="text-4xl font-semibold mt-4 font-gajraj">{band.name}</h2>
-
           {/* VIS SCENE, START OG SLUT TIDSPUNKT FOR BANDS */}
           {band.scene && band.start && band.end && (
             <p className="mt-2 text-lg font-genos">
               {band.scene} - Kl. {band.start} til {band.end}
             </p>
           )}
-
           <Link href={`/program/${band.slug}`} className="mt-4 inline-block bg-white text-black px-4 py-2 rounded-lg hover:bg-zinc-300">
             Read more
           </Link>
